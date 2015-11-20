08e9cc211b66e6450c12912ab3270893d9345dae
Rohan Varma <rohan@prompthire.com>
2015-11-20 01:07:31 -0500

    lots of shit

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 
 function moveCurrentToView() {
-	$(".slide").css("top", "0px");
+	// $(".slide").css("top", "0px");
 	var page_height = $(window).height();
-	$(".slide").not(".current").css("top", page_height + "px").attr("data-top", page_height);
+	// $(".slide").not(".current").css("top", page_height + "px").attr("data-top", page_height);
 	var current_height = $(".slide.current").height();
 	var current_top = (page_height - current_height) / 2;
-	$(".slide.current").css("top", current_top + "px").attr("data-top", current_top);
+	$(".slide.current").css("margin-top", current_top + "px");
 }
 
 function handleScroll() {
@@ -36,9 +36,25 @@ function handleScroll() {
 	}
 }
 
+var SET = false;
+
 function main(x) {
-	moveCurrentToView()
-	$(window).scroll(handleScroll)
+	// moveCurrentToView()
+	// $(window).scroll(handleScroll)
+	// $(window).scroll(function() {
+	// 	var st = $(window).scrollTop();
+	// 	var wh = $(window).height()
+	// 	var pos = $("#story-title").offset().top;
+	// 	console.log(pos, wh);
+	// 	var top = pos - wh;
+	// 	if (!SET && st > wh) {
+	// 		$("#story-title").css("position", "fixed").css("top", top + "px");
+	// 		SET = true;
+	// 	} else if (SET) {
+	// 		$("#story-title").css("position", "relative").css("top", "initial");
+	// 		SET = false;
+	// 	}
+	// })
 }
 
 $(document).ready(main)
